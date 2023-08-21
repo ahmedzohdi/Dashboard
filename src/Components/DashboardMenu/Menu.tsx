a72c727b2112044a42537363dcfac263148444fb
ahmedzohdi <ahmedzohdi97@gmail.com>
2023-08-21 17:31:17 +0200

    add files

--- src/Components/DashboardMenu/Menu.tsx
+++ src/Components/DashboardMenu/Menu.tsx
@@ -1,12 +1,56 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { IoMdHome } from "react-icons/io";
+import { AiOutlineClose } from "react-icons/ai";
+import { BiTask } from "react-icons/bi";
 import classes from "./Menu.module.css";
 
-const Menu = () => {
+interface props {
+  showMenu: boolean;
+  setShowMenu: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Menu = ({ showMenu, setShowMenu }: props) => {
+  const [focus, setFocus] = useState<string>("home");
+
+  const focusHome = focus === "home" ? classes.focus : classes.div;
+  const homeIcon =
+    focus === "home" ? classes["change-icon-color"] : classes.icon;
+  const homeText = focus === "home" ? classes.text : classes.h3;
+
+  const focusTasks = focus === "tasks" ? classes.focus : classes.div;
+  const tasksIcon =
+    focus === "tasks" ? classes["change-icon-color"] : classes.icon;
+  const tasksText = focus === "tasks" ? classes.text : classes.h3;
+
+  const display = showMenu ? classes.menu : classes.show;
   return (
-    <div className={classes.menu}>
-      <Link to="/">Home</Link>
-      <Link to="/tasks">Tasks</Link>
+    <div className={display}>
+      <div className={classes.close}>
+        <AiOutlineClose
+          className={classes["close-icon"]}
+          onClick={() => {
+            setShowMenu(!showMenu);
+          }}
+        />
+      </div>
+      <Link to="/" className={classes.link} onClick={() => setFocus("home")}>
+        <div className={focusHome}>
+          <IoMdHome className={homeIcon} />
+          <h3 className={homeText}>Home</h3>
+        </div>
+      </Link>
+      <Link
+        to="/tasks"
+        className={classes.link}
+        onClick={() => setFocus("tasks")}
+      >
+        <div className={focusTasks}>
+          <BiTask className={tasksIcon} />
+          <h3 className={tasksText}>Tasks</h3>
+        </div>
+      </Link>
     </div>
   );
 };
--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,13 +1,24 @@
 import React from "react";
+import { useState } from "react";
 import { Outlet } from "react-router";
 import Menu from "./DashboardMenu/Menu";
+import { VscMenu } from "react-icons/vsc";
 import classes from "./Main.module.css";
 
 const Main = () => {
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+  console.log(showMenu);
   return (
     <div className={classes.main}>
-      <Menu />
+      <Menu showMenu={showMenu} setShowMenu={setShowMenu} />
       <div className={classes.content}>
+        <VscMenu
+          className={classes["menu-icon"]}
+          onClick={() => {
+            setShowMenu(!showMenu);
+          }}
+        />
+
         <Outlet />
       </div>
     </div>
